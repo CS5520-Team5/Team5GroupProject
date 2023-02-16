f641c94dff675a3bafb9176058de5da3e6de3039
yuchishi <yuchixciii@gmail.com>
2023-02-15 19:04:23 -0800

    create app overall layout

---
+++ app/src/main/java/edu/northeastern/groupproject/WebServiceActivity.java
@@ -0,0 +1,14 @@
+package edu.northeastern.groupproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WebServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_service);
+    }
+}
