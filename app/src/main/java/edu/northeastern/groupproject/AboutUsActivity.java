546d776d536f78aa58e498925e1549a8bce79030
huacong <cong.h@northeastern.edu>
2023-02-28 18:08:31 -0800

    Add the about button to show group member info

--- app/src/main/java/edu/northeastern/groupproject/AboutUsActivity.java
+++ app/src/main/java/edu/northeastern/groupproject/AboutUsActivity.java
@@ -5,12 +5,12 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutUsActivity extends AppCompatActivity {
-    private TextView aboutMe;
+    private TextView aboutUs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
-        aboutMe = (TextView) findViewById(R.id.text_about);
+        aboutUs = (TextView) findViewById(R.id.text_about);
     }
 }
