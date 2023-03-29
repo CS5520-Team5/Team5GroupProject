a7d0bfc9d9e77a0a3bbee4ecf6ca859a175218d1
Yuchi Shi <yuchishi@Yuchis-MacBook-Pro.local>
2023-03-29 04:40:53 -0700

    Created news, post and profile fragment and functional button navigations

---
+++ app/src/main/java/edu/northeastern/groupproject/GameSphere/NewsFragment.java
@@ -0,0 +1,20 @@
+package edu.northeastern.groupproject.GameSphere;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.northeastern.groupproject.R;
+
+public class NewsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_news_fragment, container, false);
+    }
+    }
