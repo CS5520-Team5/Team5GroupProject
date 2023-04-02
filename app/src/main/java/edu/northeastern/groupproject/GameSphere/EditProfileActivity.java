406ad7adad9f51df27570484e11e819b154362f1
linusguo <guo.yus@northeastern.edu>
2023-04-01 23:45:21 -0700

    Updated edit profile activity

---
+++ app/src/main/java/edu/northeastern/groupproject/GameSphere/EditProfileActivity.java
@@ -0,0 +1,14 @@
+package edu.northeastern.groupproject.GameSphere;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import edu.northeastern.groupproject.R;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}
--- app/src/main/java/edu/northeastern/groupproject/GameSphere/ProfileActivity.java
+++ app/src/main/java/edu/northeastern/groupproject/GameSphere/ProfileActivity.java
@@ -2,15 +2,31 @@ package edu.northeastern.groupproject.GameSphere;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import edu.northeastern.groupproject.R;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    ImageButton btnEdit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        btnEdit = (ImageButton) findViewById(R.id.editButton);
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProfileActivity.this, EditProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
