b3179bb7ca56e76f2ee270439bd0e7d0b70a9c00
linusguo <guo.yus@northeastern.edu>
2023-04-01 21:23:46 -0700

    Refactored profile to a new activity

---
+++ app/src/main/java/edu/northeastern/groupproject/GameSphere/ProfileActivity.java
@@ -0,0 +1,16 @@
+package edu.northeastern.groupproject.GameSphere;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.northeastern.groupproject.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
