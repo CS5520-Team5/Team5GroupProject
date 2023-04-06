78ee34dd7ca49e5d82a23ba5c6aeb86a8885bd2b
YananDong001098074“git config --global user.email dong.yana@northeastern.edu“ <dong.yana@northeastern.edu>
2023-04-06 02:01:12 -0700

    register, login, logout

--- app/src/main/java/edu/northeastern/groupproject/GameSphere/GameSphereLoginActivity.java
+++ app/src/main/java/edu/northeastern/groupproject/GameSphere/GameSphereLoginActivity.java
@@ -6,23 +6,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import edu.northeastern.groupproject.R;
-import edu.northeastern.groupproject.Sticker.ConversationActivity;
-import edu.northeastern.groupproject.Sticker.LoginActivity;
 import edu.northeastern.groupproject.Sticker.User;
 
 public class GameSphereLoginActivity extends AppCompatActivity {
--- app/src/main/java/edu/northeastern/groupproject/MainActivity.java
+++ app/src/main/java/edu/northeastern/groupproject/MainActivity.java
@@ -7,7 +7,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import edu.northeastern.groupproject.FirebaseAuth.Login;
 import edu.northeastern.groupproject.GameSphere.GameSphereLoginActivity;
 import edu.northeastern.groupproject.Sticker.LoginActivity;
 import edu.northeastern.groupproject.WebService.WebServiceActivity;
@@ -15,6 +20,11 @@ import edu.northeastern.groupproject.WebService.WebServiceActivity;
 public class MainActivity extends AppCompatActivity {
 
     Button btnWebService, btnAboutUs, btnSendASticker, btnGameSphere;
+
+    FirebaseAuth auth;
+    Button button;
+    TextView textView;
+    FirebaseUser user;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -58,5 +68,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        auth = FirebaseAuth.getInstance();
+        button = findViewById(R.id.logout);
+        textView = findViewById(R.id.user_details);
+        user = auth.getCurrentUser();
+        if (user == null){
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+            finish();
+        }
+        else {
+            textView.setText(user.getEmail());
+        }
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
