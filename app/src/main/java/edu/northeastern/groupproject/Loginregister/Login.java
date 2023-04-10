543f44cd36f6c01ddfcaf54511d3935f8602c633
huacong <cong.h@northeastern.edu>
2023-04-10 12:24:29 -0700

    Make each like button be able to be clicked once for each user

--- app/src/main/java/edu/northeastern/groupproject/Loginregister/Login.java
+++ app/src/main/java/edu/northeastern/groupproject/Loginregister/Login.java
@@ -2,23 +2,21 @@ package edu.northeastern.groupproject.Loginregister;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import edu.northeastern.groupproject.MainActivity;
 import edu.northeastern.groupproject.R;
+import edu.northeastern.groupproject.GameSphere.Home;
 
 public class Login extends AppCompatActivity {
     DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://team5-9cb36-default-rtdb.firebaseio.com/");
@@ -39,40 +37,31 @@ public class Login extends AppCompatActivity {
                 final String phoneTxt = phone.getText().toString();
                 final String passwordTxt = password.getText().toString();
 
-                if(phoneTxt.isEmpty() || passwordTxt.isEmpty()){
+                if (phoneTxt.isEmpty() || passwordTxt.isEmpty()) {
                     Toast.makeText(Login.this, "Please enter your mobile and password", Toast.LENGTH_SHORT).show();
-                }
-                else{
-
+                } else{
                     databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
                         @Override
                         public void onDataChange(@NonNull DataSnapshot snapshot) {
-                            //check if mobile/phone is exist in firebase database
                             if(snapshot.hasChild(phoneTxt)){
-                                //mobile is exist in firebase database
-                                //now get password of user from firebase data and match it with user entered password
                                 final String getPassword = snapshot.child(phoneTxt).child("password").getValue(String.class);
-
-                                if(getPassword.equals(passwordTxt)){
+                                if (getPassword.equals(passwordTxt)) {
                                     Toast.makeText(Login.this, "Successfully Logged in", Toast.LENGTH_SHORT).show();
-
-                                    //open MainActivity on success
-                                    startActivity(new Intent(Login.this, MainActivity.class));
+                                    String userName = snapshot.child(phoneTxt).child("fullname").getValue(String.class);
+                                    SharedPreferences sharedPreferences = getSharedPreferences("user", MODE_PRIVATE);
+                                    sharedPreferences.edit().putString("name", userName).apply();
+                                    startActivity(new Intent(Login.this, Home.class));
                                     finish();
-                                }
-                                else{
+                                } else {
                                     Toast.makeText(Login.this, "Wrong Password", Toast.LENGTH_SHORT).show();
                                 }
-                            }
-                            else{
+                            } else {
                                 Toast.makeText(Login.this, "Wrong Mobile Number", Toast.LENGTH_SHORT).show();
                             }
                         }
 
                         @Override
-                        public void onCancelled(@NonNull DatabaseError error) {
-
-                        }
+                        public void onCancelled(@NonNull DatabaseError error) {}
                     });
                 }
             }
@@ -81,7 +70,6 @@ public class Login extends AppCompatActivity {
         registerNowBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 //open Register activity
                 startActivity(new Intent(Login.this, Register.class));
             }
