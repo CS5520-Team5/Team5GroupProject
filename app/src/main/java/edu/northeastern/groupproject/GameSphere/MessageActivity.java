dd6bf0ed3bfce35373a1e08863c320ffd91a8e2a
zhuoyahuang97 <huang.zhu@northeastern.edu>
2023-04-13 17:26:48 -0700

    check notice

--- app/src/main/java/edu/northeastern/groupproject/GameSphere/MessageActivity.java
+++ app/src/main/java/edu/northeastern/groupproject/GameSphere/MessageActivity.java
@@ -2,10 +2,19 @@ package edu.northeastern.groupproject.GameSphere;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -29,6 +38,8 @@ import edu.northeastern.groupproject.GameSphere.adapter.MessageAdapter;
 import edu.northeastern.groupproject.GameSphere.model.Member;
 import edu.northeastern.groupproject.GameSphere.model.Message;
 import edu.northeastern.groupproject.R;
+import edu.northeastern.groupproject.Sticker.ConversationActivity;
+import edu.northeastern.groupproject.Sticker.Sticker;
 
 public class MessageActivity extends AppCompatActivity {
     private String roomId,roomName;
@@ -42,6 +53,7 @@ public class MessageActivity extends AppCompatActivity {
     private HashSet<String> memberKey;
     private DatabaseReference dataRef;
     private HashMap<String,Member> memberMap;
+    private NotificationManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,6 +89,8 @@ public class MessageActivity extends AppCompatActivity {
         messageAdapter=new MessageAdapter(messageList,this);
         messageRecyclerView.setAdapter(messageAdapter);
         getMessageData();
+
+        checkNotice();
 
     }
     private void getMessageData(){
@@ -147,4 +161,76 @@ public class MessageActivity extends AppCompatActivity {
         dataRef.child("MessageHistory").push().setValue(hashMap);
 
     }
+    boolean isFirst;
+    private void checkNotice() {
+        manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel = new NotificationChannel("notice","sticker",NotificationManager.IMPORTANCE_HIGH);
+
+            manager.createNotificationChannel(notificationChannel);
+        }
+        DatabaseReference stickerRef = FirebaseDatabase.getInstance().getReference("StickerHistory");
+        stickerRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    Sticker sticker = snapshot.getValue(Sticker.class);
+                    if(sticker.getRecipient().equals(userName) ){
+                        noticeMsg(sticker);
+                    }
+                }
+                isFirst = false;
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+    private long latTime;
+    private void noticeMsg(Sticker sticker) {
+        if (latTime>sticker.getTime()){
+            return;
+        }
+        if (isFirst){
+            if (latTime<sticker.getTime()){
+                latTime = sticker.getTime();
+            }
+            return;
+        }
+        int ic = -1;
+        if (sticker.getImageId().equals("like")){
+            ic = R.drawable.ic_like;
+        } else if (sticker.getImageId().equals("kiss")){
+            ic = R.drawable.ic_kiss;
+        }else if (sticker.getImageId().equals("think")){
+            ic = R.drawable.ic_think;
+        }else if (sticker.getImageId().equals("happy")){
+            ic = R.drawable.ic_happy;
+        }else if (sticker.getImageId().equals("wipe")){
+            ic = R.drawable.ic_wipe;
+        }else if (sticker.getImageId().equals("star")){
+            ic = R.drawable.ic_star;
+        }
+        Intent intent = new Intent(this, ConversationActivity.class);
+        PendingIntent pendingIntent;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
+            pendingIntent = PendingIntent.getActivity(this, 123, intent, PendingIntent.FLAG_IMMUTABLE);
+        } else {
+            pendingIntent = PendingIntent.getActivity(this, 123, intent, PendingIntent.FLAG_ONE_SHOT);
+        }
+        Notification notification = new NotificationCompat.Builder(this,"notice")
+                .setContentText("New sticker added to chat!")
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(ic)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(),ic))
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .build();
+
+        manager.notify(1,notification);
+    }
+
 }
